6369b41abefba63bed3f1d873772815741d0aef8
Eduardo Andrade <100982195+Duhandrade22@users.noreply.github.com>
2022-05-03 00:33:37 -0300

    Update script.js

--- exercicios/Aula 14/modelo/script.js
+++ exercicios/Aula 14/modelo/script.js
@@ -5,7 +5,7 @@ function carregar() {
     var data = new Date()
     //var hora = data.getHours()
 
-    var hora = 21
+    var hora = 10
     msg.innerHTML = `Agora são ${hora} horas`
 
     if (hora >= 0 && hora < 12) {
