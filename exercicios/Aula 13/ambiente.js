6777ecc5505861dd8cd550457388ea5622628059
Eduardo Andrade <100982195+Duhandrade22@users.noreply.github.com>
2022-05-06 16:33:28 -0300

    Update ambiente.js

--- exercicios/Aula 13/ambiente.js
+++ exercicios/Aula 13/ambiente.js
@@ -1,17 +1,18 @@
-/*var c = 1
+var c = 1
 do {
     console.log (`Passo ${c}`)
     c++
-} while (c <= 5)
-*/
+} while (c <= 9)
 
 
 
 
 
 
+/*
 var c = 1
 while (c <= 6) {
     console.log ('Tudo bem?')
     c++
 }
+*/
