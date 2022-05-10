6010c3b8af138fc0244aaa133538b52dc98fa866
Eduardo Andrade <100982195+Duhandrade22@users.noreply.github.com>
2022-05-10 17:49:19 -0300

    Update script.js

--- exercicios/Aula 14ex/ex016/script.js
+++ exercicios/Aula 14ex/ex016/script.js
@@ -1,19 +1,19 @@
-function contar () {
+function contar() {
     let inicio = document.getElementById('txtinicio')
     let fim = document.getElementById('txtfim')
     let passo = document.getElementById('txtpasso')
     let res = document.getElementById('res')
 
     if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert('[ERRO] Faltam dados')
+        window.alert('[ERRO] Faltam Dados')
     } else {
-       res.innerHTML = 'Contando: '
-       let i = Number(inicio.value)
-       let f = Number(fim.value)
-       let p = Number(passo.value)
+        res.innerHTML = 'Contando: '
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
 
-       for(let c = i; c <= f; c += p) {
-           res.innerHTML += `${c} `
-       }
+        for(let c = i; c <= f; c += p) {
+            res.innerHTML += `${c}`
+        }
     }
 }
